120356ce2aaebb7e5308db104dc7d4f1d5db3f6a
Авагян Арман <69074875+Aravaman@users.noreply.github.com>
2020-11-06 18:11:46 +0500

    Buttons

---
+++ GDRAGON/Assets/Scripts/ButtonShop.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonShop : MonoBehaviour
+{
+    // Переносит на сцену с магазином
+    public void OnClickStart()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    // Переносит на сцену выбора лабы
+    public void OnClickStartTwo()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    // Возвращает в меню
+    public void OnClickBack()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
