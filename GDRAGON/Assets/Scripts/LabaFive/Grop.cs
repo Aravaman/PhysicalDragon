07b3d4e5a5ea6014e95e845f95a86bfbf71468cf
Авагян Арман <69074875+Aravaman@users.noreply.github.com>
2020-12-18 03:23:44 +0500

    Po4tiFinflFive

---
+++ GDRAGON/Assets/Scripts/LabaFive/Grop.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Grop : MonoBehaviour
+{
+    public Image im;
+    public int op;
+    public GameObject panelOne;
+
+    void Update()
+    {
+        string str = "" + op;
+        if (Gem.scoreCount >= op)
+        {
+            im.sprite = Resources.Load<Sprite>(str);
+        }
+
+        if (Gem.scoreCount >= 4)
+        {
+            StartCoroutine(ExampleCoroutine());
+        }
+    }
+
+    IEnumerator ExampleCoroutine()
+    {
+        yield return new WaitForSeconds(3.0f);
+        panelOne.SetActive(true);
+    }
+}
