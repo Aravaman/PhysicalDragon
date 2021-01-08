dfd8aba131d27b9699848b443d759935f9da15ef
Авагян Арман <69074875+Aravaman@users.noreply.github.com>
2021-01-09 03:53:49 +0500

    minusfour

---
+++ GDRAGON/Assets/Scripts/LabaThre/PlusOne.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlusOne : MonoBehaviour
+{
+    public GameObject panelOne;
+
+    public void OnClicPlus()
+    {
+        if (Gem.scoreCount >= 0 || Gem.scoreCount < 3)
+            Gem.scoreCount += 1;
+    }
+
+    void Update()
+    {
+        if (Gem.scoreCount == 3)
+            panelOne.SetActive(false);
+}
+}
---
+++ GDRAGON/Assets/Scripts/LabaThre/ToFinalThree.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class ToFinalThree : MonoBehaviour
+{
+    public GameObject panelOne;
+    public GameObject panelTwo;
+
+    void Update()
+    {
+        if (Gem.scoreCount >= 13)
+            StartCoroutine(ExampleCoroutine());
+    }
+
+    IEnumerator ExampleCoroutine()
+    {
+        yield return new WaitForSeconds(3f);
+        if (Gem.scoreCount == 13 || Gem.scoreCount == 15)
+            panelTwo.SetActive(true);
+        if (Gem.scoreCount == 14)
+            panelOne.SetActive(true);
+    }
+}
